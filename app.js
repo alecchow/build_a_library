cef30e23aba6a3864b3280fcc9130bc537c87d58
alecchow <84516588+alecchow@users.noreply.github.com>
2021-06-08 16:42:51 +0100

    Create new instance to test Book class

--- app.js
+++ app.js
@@ -67,3 +67,7 @@ class Movie extends Media {
       return this.runTime;
     }
   }
+
+  /* Testing */
+
+  const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
