b433aaf8be4691b99b58d1bf17adb50ad9521f57
alecchow <84516588+alecchow@users.noreply.github.com>
2021-06-08 16:35:20 +0100

    Create getter and setter

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ class Media {
         this._isCheckedOut = false;
         this._ratings = [];
     }
+
+    get title() {
+        return this._title;
+    }
+
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+    
+    set isCheckedOut(status) {
+        this._ischeckedOut = status;
+    }
+
+    get ratings() {
+        return this._ratings;
+    }
