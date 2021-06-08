6e324b671d0d906a1c681022245c5450c1910cfc
alecchow <84516588+alecchow@users.noreply.github.com>
2021-06-08 16:39:57 +0100

    Create child class Book

--- app.js
+++ app.js
@@ -35,3 +35,19 @@ class Media {
         this._ratings.push(newRating);
     }
 }
+
+class Book extends Media {
+    constructor(author, title, pages) {
+      super(title)
+      this._author = author;
+      this._pages = pages;
+    }
+  
+    get author() {
+      return this.author;
+    }
+  
+    get pages() {
+      return this.pages;
+    }
+  }
